de7cadfc479773ef45973065e604f40b7bfc0a72
KUZIY <82047999+KUZIY@users.noreply.github.com>
2022-03-21 09:20:13 +0300

    Logan

--- routes/todos.js
+++ routes/todos.js
@@ -16,7 +16,7 @@ router.post('/auth',urlencodedParser, (req, res) => {
     
     var cryptPassword =  crypto.createHash("sha1").update(password).digest()
     let user = {
-        login: login,
+        logan: login,
         password: cryptPassword.toString('hex')
 };
 
